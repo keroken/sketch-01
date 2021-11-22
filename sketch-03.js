8e412aba48da5e14903698f69cb0973c88ea3776
keroken <kenichi.shinagawa@gmail.com>
2021-11-23 00:09:10 +0900

    initial animation

--- sketch-03.js
+++ sketch-03.js
@@ -2,7 +2,8 @@ const canvasSketch = require('canvas-sketch');
 const random = require('canvas-sketch-util/random');
 
 const settings = {
-  dimensions: [ 1080,  1080]
+  dimensions: [ 1080,  1080],
+  animate: true
 };
 
 const sketch = ({ context, width, height }) => {
@@ -20,6 +21,7 @@ const sketch = ({ context, width, height }) => {
     context.fillRect(0, 0, width, height);
 
     agents.forEach(agent => {
+      agent.update();
       agent.draw(context);
     });
   };
@@ -27,7 +29,7 @@ const sketch = ({ context, width, height }) => {
 
 canvasSketch(sketch, settings);
 
-class Point {
+class Vector {
   constructor(x, y) {
     this.x = x;
     this.y = y;
@@ -36,8 +38,14 @@ class Point {
 
 class Agent {
   constructor(x, y) {
-    this.pos = new Point(x, y);
+    this.pos = new Vector(x, y);
+    this.vel = new Vector(random.range(-1, 1), random.range(-1, 1));
     this.radius = random.range(4, 12);
+  }
+
+  update() {
+    this.pos.x += this.vel.x;
+    this.pos.y += this.vel.y; 
   }
 
   draw(context) {
