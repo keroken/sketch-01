0a64caa94e6414a5a1620946dc44034d62dd484f
keroken <kenichi.shinagawa@gmail.com>
2021-11-22 23:26:12 +0900

    sketch-03 create agent class

--- sketch-03.js
+++ sketch-03.js
@@ -9,26 +9,34 @@ const sketch = () => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
-    const pointA = new Point(800, 400, 10);
-    const pointB = new Point(300, 700, 10);
+    const agentA = new Agent(800, 400);
+    const agentB = new Agent(300, 700);
 
-    context.beginPath();
-    context.arc(pointA.x, pointA.y, pointA.radius, 0, Math.PI * 2);
-    context.fillStyle = 'black';
-    context.fill();
-
-    context.beginPath();
-    context.arc(pointB.x, pointB.y, pointB.radius, 0, Math.PI * 2);
-    context.fill();
+    agentA.draw(context);
+    agentB.draw(context);
   };
 };
 
 canvasSketch(sketch, settings);
 
 class Point {
-  constructor(x, y, radius) {
+  constructor(x, y) {
     this.x = x;
     this.y = y;
-    this.radius = radius;
+  }
+}
+
+class Agent {
+  constructor(x, y) {
+    this.pos = new Point(x, y);
+    this.radius = 10;
+  }
+
+  draw(context) {
+    context.fillStyle = 'black';
+
+    context.beginPath();
+    context.arc(this.pos.x, this.pos.y, this.radius, 0, Math.PI * 2);
+    context.fill();
   }
 }
